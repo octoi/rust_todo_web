728d45b3d639df2b144456b85a9b9f404f4cda8a
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-30 08:11:47 +0530

    security infatructure

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,17 +1,22 @@
 use super::{TodoMac, TodoPatch};
-use crate::model::{db::init_db, todo::TodoStatus};
+use crate::{
+    model::{db::init_db, todo::TodoStatus},
+    security::utx_from_token,
+};
 
 #[tokio::test]
 async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
     // -- FIXTURE
     let db = init_db().await?;
+    let utx = utx_from_token("123").await?;
+
     let data_fx = TodoPatch {
         title: Some("test - model_todo_create_1".to_string()),
         ..Default::default()
     };
 
     // -- ACTION
-    let todo_created = TodoMac::create(&db, data_fx.clone()).await?;
+    let todo_created = TodoMac::create(&db, &utx, data_fx.clone()).await?;
 
     // -- CHECK
     assert!(todo_created.id >= 1000, "Id should be >= 1000");
@@ -25,9 +30,10 @@ async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
 async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
     // -- FIXTURE
     let db = init_db().await?;
+    let utx = utx_from_token("123").await?;
 
     // -- ACTION
-    let todos = TodoMac::list(&db).await?;
+    let todos = TodoMac::list(&db, &utx).await?;
 
     // -- CHECK
     assert_eq!(2, todos.len());
