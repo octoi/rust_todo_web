764297d6676dcd5aeb6e35e67ea2e28371a7f377
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-30 08:05:57 +0530

    TEST: todo create

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,6 +1,25 @@
-use crate::model::db::init_db;
+use super::{TodoMac, TodoPatch};
+use crate::model::{db::init_db, todo::TodoStatus};
 
-use super::TodoMac;
+#[tokio::test]
+async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
+    // -- FIXTURE
+    let db = init_db().await?;
+    let data_fx = TodoPatch {
+        title: Some("test - model_todo_create_1".to_string()),
+        ..Default::default()
+    };
+
+    // -- ACTION
+    let todo_created = TodoMac::create(&db, data_fx.clone()).await?;
+
+    // -- CHECK
+    assert!(todo_created.id >= 1000, "Id should be >= 1000");
+    assert_eq!(data_fx.title.unwrap(), todo_created.title);
+    assert_eq!(TodoStatus::Open, todo_created.status);
+
+    Ok(())
+}
 
 #[tokio::test]
 async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
