39247e972e3fc7a122453fdb841f2a6aa1265425
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-30 08:42:30 +0530

    tests

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,6 +1,6 @@
 use super::{TodoMac, TodoPatch};
 use crate::{
-    model::{db::init_db, todo::TodoStatus},
+    model::{self, db::init_db, todo::TodoStatus},
     security::utx_from_token,
 };
 
@@ -22,6 +22,73 @@ async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
     assert!(todo_created.id >= 1000, "Id should be >= 1000");
     assert_eq!(data_fx.title.unwrap(), todo_created.title);
     assert_eq!(TodoStatus::Open, todo_created.status);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn model_todo_get_ok() -> Result<(), Box<dyn std::error::Error>> {
+    // -- FIXTURE
+    let db = init_db().await?;
+    let utx = utx_from_token("123").await?;
+
+    // -- ACTION
+    let todo = TodoMac::get(&db, &utx, 100).await?;
+
+    // -- CHECK
+    assert_eq!(100, todo.id);
+    assert_eq!("todo 100", todo.title);
+    assert_eq!(TodoStatus::Close, todo.status);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn model_todo_get_wrong_id() -> Result<(), Box<dyn std::error::Error>> {
+    // -- FIXTURE
+    let db = init_db().await?;
+    let utx = utx_from_token("123").await?;
+
+    // -- ACTION
+    let result = TodoMac::get(&db, &utx, 999).await;
+
+    // -- CHECK
+    match result {
+        Ok(_) => assert!(false, "Should not succeed"),
+        Err(model::Error::EntityNotFound(typ, id)) => {
+            assert_eq!("todo", typ);
+            assert_eq!(999.to_string(), id)
+        }
+        other_error => assert!(false, "Wrong Error {:?}", other_error),
+    }
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn model_todo_update_ok() -> Result<(), Box<dyn std::error::Error>> {
+    // -- FIXTURE
+    let db = init_db().await?;
+    let utx = utx_from_token("123").await?;
+    let data_fx = TodoPatch {
+        title: Some("test - model_todo_update_ok 1".to_string()),
+        ..Default::default()
+    };
+
+    let todo_fx = TodoMac::create(&db, &utx, data_fx).await?;
+    let update_data_fx = TodoPatch {
+        title: Some("test - model_todo_update_ok 2".to_string()),
+        ..Default::default()
+    };
+
+    // -- ACTION
+    let todo_updated = TodoMac::update(&db, &utx, todo_fx.id, update_data_fx.clone()).await?;
+
+    // -- CHECK
+    let todos = TodoMac::list(&db, &utx).await?;
+    assert_eq!(3, todos.len());
+    assert_eq!(todo_fx.id, todo_updated.id);
+    assert_eq!(update_data_fx.title.unwrap(), todo_updated.title);
 
     Ok(())
 }
