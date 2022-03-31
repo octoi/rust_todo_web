912b8ff7e2c4ca93ae6c988ce39d607a831f2569
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-31 19:56:59 +0530

    running server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,32 @@
 #![allow(unused)] // silence unused warnings while exploring (to comment out)
 
+use std::{env, sync::Arc};
+
+use model::init_db;
+use web::start_web;
+
 mod model;
 mod security;
 mod web;
 
-fn main() {
-    println!("Hello, world!");
+const DEFAULT_WEB_FOLDER: &'static str = "web_folder/";
+const DEFAULT_WEB_PORT: u16 = 8080;
+
+#[tokio::main]
+async fn main() {
+    // compute the web_folder
+    let mut args: Vec<String> = env::args().collect();
+    let web_folder = args.pop().unwrap_or_else(|| DEFAULT_WEB_FOLDER.to_string());
+    let web_port = DEFAULT_WEB_PORT;
+
+    // get the database
+    // TODO: loop until valid DB
+    let db = init_db().await.expect("Cannot init db");
+    let db = Arc::new(db);
+
+    // start the server
+    match start_web(&web_folder, web_port, db).await {
+        Ok(_) => println!("Server ended"),
+        Err(ex) => println!("ERROR - web server failed to start. Cause {:?}", ex),
+    }
 }
