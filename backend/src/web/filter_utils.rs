ee91b5ee63391c1b4b240d786d677f90e37c0b76
fadhilsaheer <fadhilsaheer@gmail.com>
2022-04-03 15:23:56 +0530

    refactor

--- backend/src/web/filter_utils.rs
+++
@@ -1,9 +0,0 @@
-use std::{convert::Infallible, sync::Arc};
-
-use warp::Filter;
-
-use crate::model::Db;
-
-pub fn with_db(db: Arc<Db>) -> impl Filter<Extract = (Arc<Db>,), Error = Infallible> + Clone {
-    warp::any().map(move || db.clone())
-}
