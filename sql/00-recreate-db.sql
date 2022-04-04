4e35279868a3d4ee4fe8c0330dfce9b33dd9da25
fadhilsaheer <fadhilsaheer@gmail.com>
2022-04-04 08:05:28 +0530

    commented DB drop

--- sql/00-recreate-db.sql
+++ sql/00-recreate-db.sql
@@ -1,5 +1,5 @@
 -- DEV ONLY - Comment out for keeping db between restart
-DROP DATABASE IF EXISTS app_db;
+-- DROP DATABASE IF EXISTS app_db;
 DROP USER IF EXISTS app_user;
 
 -- DEV ONLY - for quick iteration
